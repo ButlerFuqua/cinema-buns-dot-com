f20d37850208ceeb2e47ac0ba018135798df3109
butlerfuqua <butlerfuqua@gmail.com>
2025-10-26 20:44:03 -0400

    Add support for hosts and connect modals

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,11 +2,47 @@
 	import { page } from "$app/state";
 	import logo from "$lib/images/svelte-logo.svg";
 	import github from "$lib/images/github.svg";
+    import Modal from "./Modal.svelte";
+    import SocialLinks from "./SocialLinks.svelte";
+
+	let showHostsModal = false;
+	let showConnectModal = false;
 </script>
 
 <header>
-	<a href="#">Connect</a>
-	<a href="#">Ask a question</a>
+	<a href="#" on:click={() => showConnectModal = true}>Connect</a>
+	<a href="#" on:click={() => showHostsModal = true}>Hosts</a>
+	<a href="#" >Ask a question</a>
+
+	<Modal isOpen={showHostsModal} onClose={() => (showHostsModal = false)}>
+        <div class="modal-content">
+            <h2>Hosted by...</h2>
+            <div class="hosts-container">
+                <div class="host-blurb">
+					<img src="/cops_vs_ninjas_logo.png" alt="Scott Wood Profile" />
+					<h2>Scott Wood</h2>
+					<p>Scott Wood is a person. he is the host of the show. That is who is is, and that is what he does, relative to this podcast and website.</p>
+				</div>
+                <div class="host-blurb">
+					<img src="/cops_vs_ninjas_logo.png" alt="Scott Wood Profile" />
+					<h2>Scott Wood</h2>
+					<p>Scott Wood is a person. he is the host of the show. That is who is is, and that is what he does, relative to this podcast and website.</p>
+				</div>
+            </div>
+        </div>
+    </Modal>
+	<Modal isOpen={showConnectModal} onClose={() => (showConnectModal = false)}>
+		<br />
+
+		<!-- EMAIL SUBSCRIBE FORM HERE -->
+		 
+		<!-- EMAIL SUBSCRIBE FORM HERE -->
+        <div class="modal-content">
+            <h2 style="text-align: center;">Follow Cinema Buns on...</h2>
+			<br />
+            <SocialLinks />
+        </div>
+    </Modal>
 </header>
 
 <style>
@@ -46,5 +82,19 @@
 		color: #fefefe;
 		background-color: var(--default-bg);
 		text-align: right;
+	}
+
+	.hosts-container{
+		display: flex;
+		flex-direction: column;
+	}
+	.host-blurb {
+		display: flex;
+		flex-direction: column;
+		margin: 1rem auto;
+	}
+	.host-blurb img {
+		max-width: 100%;
+		border-radius: 5px;
 	}
 </style>
