d2738512f2c4b4fa06aed0c5811e806597ddf40a
butlerfuqua <butlerfuqua@gmail.com>
2025-10-27 18:53:49 -0400

    Add email subscribe form

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -34,9 +34,18 @@
 	<Modal isOpen={showConnectModal} onClose={() => (showConnectModal = false)}>
 		<br />
 
-		<!-- EMAIL SUBSCRIBE FORM HERE -->
-		 
-		<!-- EMAIL SUBSCRIBE FORM HERE -->
+		<div style="display: flex; justify-content:center; align-items:center; flex-direction:column">
+			<h2>Join our mailing list!</h2>
+		<p>We won't share your email or annoy you.</p>
+		</div>
+
+		<div style="display: flex; justify-content:center; align-items:center; flex-direction:column">
+
+			<!-- EMAIL SUBSCRIBE FORM HERE -->
+		 <script async src="https://subscribe-forms.beehiiv.com/embed.js"></script><iframe src="https://subscribe-forms.beehiiv.com/ae428906-d088-4220-8bb8-8d7490e11b2d" class="beehiiv-embed" data-test-id="beehiiv-embed" frameborder="0" scrolling="no" style="width: 560px; height: 206px; margin: 0; border-radius: 0px 0px 0px 0px !important; background-color: transparent; box-shadow: 0 0 #0000; max-width: 100%;"></iframe>
+	<!-- EMAIL SUBSCRIBE FORM HERE -->
+
+		</div>
         <div class="modal-content">
             <h2 style="text-align: center;">Follow Cinema Buns on...</h2>
 			<br />
